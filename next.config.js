c1ebce04d49f64197c35959e1b182f68b262600e
Jesus Calderas <jsscalderas0@gmail.com>
2023-03-22 16:37:15 -0400

    configuration to deploy is missing

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placeimg.com',
+      },
+    ],
+    domains: ['placeimg.com'],
+  },
+}
+
+
+
+// https://api.lorem.space/image/fashion?w=640&h=480&r=1341
